6d1d753eb409f8e0ccdcec236fa549e468474407
杨希磊 <74948883+yuri02-eng@users.noreply.github.com>
2025-04-14 15:30:14 +0800

    1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # untitled
 
-This template should help get you started developing with Vue 3 in Vite.
+This template should help get you started developing with Vue 3 in Vite.for vue3
 
 ## Recommended IDE Setup
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import Applayout from './layouts/Applayout.vue'
+import router   from "@/stores/indes.js";
+try {
+    const app = createApp(Applayout)
+    
+    app.use(ElementPlus)
+    app.use(router)
+    app.mount('#app')
+} catch (error) {
+    console.error('Failed to initialize the application:', error)
+}
